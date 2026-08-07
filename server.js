b5bcd838ea7989c4b9ce0ff64e5eb2fc6bd4a8df
danielberihu72-cmd <danielberihu72@gmail.com>
2026-08-07 23:06:40 +0300

    Update Telegram bot token in server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 // 1. የቴሌግራም ቦት ቶከን
-const TOKEN = '8994032862:AAHM-hoRiyR6QR9UrnJ4YZjLi7YT27D94y8';
+const TOKEN = '8989868624:AAGTuazoUV7NvEFcMhpIxIqz-TJBb41WJcg';
 const bot = new TelegramBot(TOKEN, { polling: true });
 const WEB_APP_URL = 'https://vercel.app';
 
