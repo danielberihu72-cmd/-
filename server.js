30de046824913ebab1e45d24a1bde97066445a7c
danielberihu72-cmd <danielberihu72@gmail.com>
2026-08-07 23:13:13 +0300

    Fix formatting of SUPABASE_ANON_KEY declaration

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const WEB_APP_URL = 'https://vercel.app';
 // 2. የSupabase ግንኙነት ማዋቀሪያ
 // እባክህ እነዚህን ሁለቱን በትክክለኛ የSupabase መረጃዎችህ ተካቸው
 const SUPABASE_URL = 'https://supabase.co'; 
-const SUPABASE_ANON_KEY = 'sb_publishable_YZHcP8wEIx1KCv1-afd3jA_yKadAGEh';
+const SUPABASE_ANON_KEY = 'sb_publishable_YZHcP8wEIx1KCv1-afd3jA_yKadAGEh' ;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
